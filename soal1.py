6924f828f989b6f2b49b32f65f0e3943392ede9d
Paetttt <118429357+Paetttt@users.noreply.github.com>
2023-01-03 14:07:45 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,4 @@
+print("*******Justice League******")
+input("Masukan username anda: ")
+def c ():
+    print("====Welcome victorstone====")
---
+++ soal2.py
@@ -0,0 +1,16 @@
+print("====Selamat Datang di XXV====")
+input('Masukan tanggal hari ini: ')
+
+print("==Berikut genre Film yang tersedia==")
+print("1. Horror")
+print('2. Action')
+
+input("Silahkan pilih mau nonton film bergenre apa: ")
+if(): 1 
+print("==Berikut pilihan film horor yang sedang tayang==")
+print("1. The Devil's Light")
+print("2. Pengabdi Setan")
+if():2 
+print("==Berikut pilihan film horor yang sedang tayang==")
+print("1. Black Panther: Wakanda Forever")
+print("2. Avatar: The Way Of Wather")
